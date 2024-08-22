dc4293c5b8536b8b04270b29b020b412853cb902
Antônio Rocha <antoniojunior159@gmail.com>
2024-08-22 00:14:47 -0300

    Commit aula 05

---
+++ Aulas/Aula05/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Aula05
+{ 
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("==========Calculadora Bem Simples=========");
+            Console.WriteLine("Digite o número 1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o número 2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = number1 + number2;
+            int subtraction = number2 - number1;
+            int multiplication = number1 * number2;
+            int division = number2 / number1;
+            int module = number2 % number1;
+
+            Console.WriteLine("O valor da soma é: " + sum);
+            Console.WriteLine("O valor da subtração é: " + subtraction);
+            Console.WriteLine("O valor da multiplicação é: " + multiplication);
+            Console.WriteLine("O valor da divisão é: " + division);
+            Console.WriteLine("O resto da divisão é: " + module);
+        }
+    }
+}
