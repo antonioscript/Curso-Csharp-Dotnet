ccc5a2aabc0d788589a10b5b90f76e1e453352e2
Antônio Rocha <antoniojunior159@gmail.com>
2024-08-22 01:02:58 -0300

    Commit da aula 06

---
+++ Aulas/Aula06/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula06
+{
+    public class Program
+    { 
+        public static void Main()
+        {
+            Console.WriteLine("=============Verificador de Números Pares ou Ímpares=========");
+            Console.WriteLine("Digite um número: ");
+            int number =  Convert.ToInt32(Console.ReadLine());
+
+            int result = number % 2;
+ 
+            if (result == 0) 
+            {
+                Console.WriteLine("O número digitado é par!");
+            }
+            else
+            {
+                Console.WriteLine("O número é ímpar!");
+            }
+        }
+    }
+}
