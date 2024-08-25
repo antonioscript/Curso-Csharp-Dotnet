6021c9f16856c0cb26cc4551100722f47e635612
Antônio Rocha <antoniojunior159@gmail.com>
2024-08-24 23:59:50 -0300

    Commit da aula 07

---
+++ Aulas/Aula07/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Aula07;
+public class Program
+{ 
+    public static void Main()
+    {
+        Console.WriteLine("=======Operadores de Atribuição=======");
+
+        //Incializar a variável
+        int x = 10;
+        Console.WriteLine("\nValor Inicial da Variável de x: " + x);
+
+        //Operador de Atribuição Simples (=)
+        x = 20;
+        Console.WriteLine("\nAtribuição Simples (=)");
+        Console.WriteLine("Novo Valor de x: " + x);
+
+        //Atribuição Composta (incremento) (++)
+        x++; //Equivale a x = x + 1
+
+        Console.WriteLine("\nAtribuição Composta Incremento (++)");
+        Console.WriteLine("Novo Valor de x: " + x);
+
+        //Atribuição Composta  (+=)
+        x += 10; //Equivale x = x + 10
+
+        Console.WriteLine("\nAtribuição Composta (+=)");
+        Console.WriteLine("Novo Valor de x: " + x);
+
+        //Atribuição Composta  (-=)
+        x -= 10; //Equivale x = x - 10
+
+        Console.WriteLine("\nAtribuição Composta (-=)");
+        Console.WriteLine("Novo Valor de x: " + x);
+
+        //Atribuição Composta  (*=)
+        x *= 10; //Equivale x = x * 10
+
+        Console.WriteLine("\nAtribuição Composta (*=)");
+        Console.WriteLine("Novo Valor de x: " + x);
+
+        //Atribuição Composta  (/=)
+        x /= 10; //Equivale x = / * 10
+
+        Console.WriteLine("\nAtribuição Composta (/=)");
+        Console.WriteLine("Novo Valor de x: " + x);
+
+
+
+    }
+}
