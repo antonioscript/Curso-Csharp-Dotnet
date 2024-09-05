81a2007a4e47fa2f254aec7727ed170be9e9995a
Antônio Rocha <antoniojunior159@gmail.com>
2024-09-05 00:05:27 -0300

    Commit da Aula 12

---
+++ Aulas/Aula12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Aula12;
+public class Program
+{
+    public static void Main()
+    {
+        //Ex1
+        //int i = 0;
+
+        //while (i <= 1000)
+        //{
+        //    Console.WriteLine(i);
+               //int++
+        //}
+
+        //Ex2:
+        double budget = 100;
+
+        while (budget > 0)
+        {
+            Console.WriteLine(budget);
+            //Realizando alguma operação
+            budget--;
+        }
+
+        Console.WriteLine("Atenção! Saldo insuficiente");
+
+
+    }
+}
