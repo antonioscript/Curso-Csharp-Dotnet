52bf7eaf66120bd3cf8bdf30f6a3fb03fd59e617
Antônio Rocha <antoniojunior159@gmail.com>
2024-09-05 00:44:56 -0300

    Commit da Aula 15

---
+++ Aulas/Aula15/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula15;
+public class Program
+{
+    public static void Main()
+    {
+        //Coleções (listas)
+        List<int> numberList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        foreach (int number in numberList)
+        {
+            //Faça algo
+            Console.WriteLine(number);
+        }
+
+    }
+}
