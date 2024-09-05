1b4923ba18881b52e88f90d190431727692e5a8f
Antônio Rocha <antoniojunior159@gmail.com>
2024-09-05 00:12:54 -0300

    Commit da Aula 13

---
+++ Aulas/Aula13/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula13;
+public class Program
+{
+    public static void Main()
+    {
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= 10);
+
+    }
+}
