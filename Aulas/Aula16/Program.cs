ce3c79c96afbbae20f7a49ef5a1b3223a1d94e33
Antônio Rocha <antoniojunior159@gmail.com>
2025-01-10 00:09:47 -0300

    commit da aula 16

---
+++ Aulas/Aula16/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Aula16;
+public class Program
+{
+    public static void Main()
+    {
+        string separator = "----------------";
+
+        //1. Declaração de Array inicializando com valores
+        string[] fruits = { "Maçã", "Banana", "Caju", "Manga"};
+
+        //2. Acessar os valores
+        Console.WriteLine(fruits[0]);
+
+        Console.WriteLine(separator);
+
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        Console.WriteLine(separator);
+
+        //3. Contagem de Índices
+        Console.WriteLine("Quantidade de Índices: " + fruits.Length);
+
+        Console.WriteLine(separator);
+
+        //4. Alterar os valores
+        Console.WriteLine("Valor antes da alteração: " + fruits[1]);
+
+        fruits[1] = "Abacaxi";
+
+        Console.WriteLine("Valor depois da alteração: " + fruits[1]);
+
+        Console.WriteLine(separator);
+
+        //5. Declaração sem inicializar
+        int[] numbers = new int[4];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+    }
+}
