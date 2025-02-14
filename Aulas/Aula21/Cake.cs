2ac554c525f60c5a81f3aafbf2648a48c3affe6a
Antônio Rocha <antoniojunior159@gmail.com>
2025-02-13 23:23:32 -0300

    Commit da aula 21

---
+++ Aulas/Aula21/Cake.cs
@@ -0,0 +1,29 @@
+﻿namespace Aula21;
+public class Cake
+{
+    //1. Atributos
+    public string Nome;
+    public string Cor;
+    public string Sabor;
+    public int Camadas;
+    public bool TemRecheio;
+    public DateTime DataFabricao;
+    public decimal Valor;
+
+}
+
+
+public class Animal
+{
+    public string Nome;
+}
+
+public class Avião
+{
+
+}
+
+public class Calçado
+{ 
+
+}
---
+++ Aulas/Aula21/Car.cs
@@ -0,0 +1,14 @@
+﻿
+//Toda classe tem um namespace
+namespace Aula21;
+
+//Classe (forma) Car
+public class Car
+{
+    //1. Atributos
+    public string Color;
+    public int Year;
+    public string Model;
+    public string Brand;
+
+}
