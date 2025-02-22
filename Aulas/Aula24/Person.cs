c33cadf3ca66bb91b1e36e5b58ddcc73152af735
Antônio Rocha <antoniojunior159@gmail.com>
2025-02-22 01:25:54 -0300

    Commit da Aula 24

---
+++ Aulas/Aula24/Person.cs
@@ -0,0 +1,59 @@
+﻿namespace Aula24
+{
+    public class Person
+    {
+        //1. Atributos
+        public string Name;
+        public int Age;
+
+        //2. Métodos
+        public bool IfLegalPerson(int age)
+        {
+            if (age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //3. Refatorar
+        public string IfLegalPersonRefactor(int age)
+        {
+            string result = string.Empty;
+
+            if (age >= 18)
+            {
+                result = "A pessoa é maior de Idade!";
+                return result;
+            }
+            else
+            {
+                result = "A pessoa é menor de Idade!";
+                return result;
+            }
+
+        }
+
+        //4. Refatorado
+        public string IfLegalPersonRefactorName(int age, string name)
+        {
+            string result = string.Empty;
+
+            if (age >= 18)
+            {
+                result = "O(a) " + name + " é maior de Idade!";
+                return result;
+            }
+            else
+            {
+                result = "O(a) " + name + " é menor de Idade!";
+                return result;
+            }
+
+        }
+    }
+}
---
+++ Aulas/Aula24/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula24;
+public class Program
+{
+    public static void Main()
+    {
+        //1. Criar instância da classe Person
+        Person person = new Person();
+        person.Name = "João da Silva";
+
+        //2. Métodos
+        Console.WriteLine("Digite uma idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        //3.Chamando o método e guardando o resultado
+        string response = person.IfLegalPersonRefactorName(age, person.Name);
+
+        Console.WriteLine(response);
+
+    }
+}
