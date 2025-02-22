80b727538a92168faecc79defaef6fdc6aca0b10
Antônio Rocha <antoniojunior159@gmail.com>
2025-02-22 00:45:52 -0300

    Commit da Aula 23

---
+++ Aulas/Aula23/Bank.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula23
+{
+    public class Bank
+    {
+        //1. Atributos
+        public int Id;
+        public string Name;
+
+        //2. Métodos (sem retorno)
+        public void RequestNewCard()
+        {
+            //Lógica
+            Console.WriteLine("Enviando Email... (Solicite um novo cartão, ligando para o número: 8888-8888)");
+        }
+
+    }
+}
---
+++ Aulas/Aula23/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Aula23;
+public class Program
+{
+    public static void Main()
+    {
+        //1. Criar a instância bank
+        var bankPraiano = new Bank();
+        bankPraiano.Name = "Banco Praiano";
+        bankPraiano.Id = 02545;
+
+
+        //2. Chamando Método da instância da classe Bank
+        bankPraiano.RequestNewCard();
+    }
+}
