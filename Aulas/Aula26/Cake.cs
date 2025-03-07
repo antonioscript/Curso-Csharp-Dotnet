9fd1307c7811560a20e51f8f16197653c1c581f0
Antônio Rocha <antoniojunior159@gmail.com>
2025-03-06 22:56:41 -0300

    Commit da aula 26

---
+++ Aulas/Aula26/Cake.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula26
+{
+    public class Cake
+    {
+        public string Flavor;
+        public string Filling;
+        public string Coverage;
+        public int Layers;
+        public string Size;
+
+        public Cake()
+        {
+            Flavor = "Chocolate";
+            Filling = "Coco";
+            Coverage = "Chocolate";
+            Layers = 3;
+            Size = "Médio";
+        }
+    }
+}
---
+++ Aulas/Aula26/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Aula26;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Instância da Classe
+        Cake cake = new Cake();
+
+        Console.WriteLine($"Sabor: {cake.Flavor}");
+        Console.WriteLine($"Recheio: {cake.Filling}");
+        Console.WriteLine($"Cobertura: {cake.Coverage}");
+        Console.WriteLine($"Camadas: {cake.Layers}");
+        Console.WriteLine($"Tamanho: {cake.Size}");
+
+        
+    }
+}
