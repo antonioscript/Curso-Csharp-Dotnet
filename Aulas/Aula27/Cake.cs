18334af54c14b7f66844ee01e7ab0e6028289777
Antônio Rocha <antoniojunior159@gmail.com>
2025-03-06 23:15:49 -0300

    Commit da Aula 27

---
+++ Aulas/Aula27/Cake.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula27
+{
+    public class Cake
+    {
+        public string Flavor;
+        public string Filling;
+        public string Coverage;
+        public int Layers;
+        public string Size;
+
+        public Cake(string flavor, string filling, string coverage, int layers, string size)
+        {
+            Flavor = flavor;
+            Filling = filling;
+            Coverage = coverage;
+            Layers = layers;
+            Size = size;
+        }
+    }
+}
---
+++ Aulas/Aula27/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula27;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Instância da Classe | Chocolate
+        Cake cakeChocolate = new Cake("Chocolate", "Coco", "Chocolate", 3, "Médio");
+
+        Console.WriteLine($"Sabor: {cakeChocolate.Flavor}");
+        Console.WriteLine($"Recheio: {cakeChocolate.Filling}");
+        Console.WriteLine($"Cobertura: {cakeChocolate.Coverage}");
+        Console.WriteLine($"Camadas: {cakeChocolate.Layers}");
+        Console.WriteLine($"Tamanho: {cakeChocolate.Size}");
+
+        Console.WriteLine("--------------------");
+
+        //Instância da Classe | Morango
+        Cake cakeStrawberry = new Cake("Morango", "Moragno", "Chocolate", 1, "Pequeno");
+
+        Console.WriteLine($"Sabor: {cakeStrawberry.Flavor}");
+        Console.WriteLine($"Recheio: {cakeStrawberry.Filling}");
+        Console.WriteLine($"Cobertura: {cakeStrawberry.Coverage}");
+        Console.WriteLine($"Camadas: {cakeStrawberry.Layers}");
+        Console.WriteLine($"Tamanho: {cakeStrawberry.Size}");
+
+    }
+}
